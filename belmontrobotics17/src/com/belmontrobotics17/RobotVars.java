e5c97b459f2c5e873e8b32491d60ca013128fa1e
Vassy Lord of Arda <Vassy Lord of Arda@DESKTOP-GVK2MQ5>
2017-01-20 17:52:46 -0800

    robotvars

---
+++ belmontrobotics17/src/com/belmontrobotics17/RobotVars.java
@@ -0,0 +1,16 @@
+package com.belmontrobotics17;
+
+public class RobotVars {
+
+	// Drive PID constants
+	public static double DRIVE_PID_P = 1.0;
+	public static double DRIVE_PID_I = 0.0;
+	public static double DRIVE_PID_D = 0.0;
+	
+	public static double DRIVE_PID_TOLERANCE = 0.5;
+
+	// Controller constants
+	public static double CHEESY_ROTATION_SENS = 1.0;
+	public static double CHEESY_ROTATION_SENS_FAST = 2.0;
+	public static double CHEESY_CONTROL_SENS = 1.0;
+}
